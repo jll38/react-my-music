0e616a7e83e2cedc23e2552d9ceb7e1943345fc7
jll38 <jll38@njit.edu>
2022-11-29 19:50:13 -0500

    flex slider cards

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Cards
+ from './components/cards';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div className='slider'>
-          <div className="card">
-            <div className="card-img">
-              <img src="https://i1.sndcdn.com/artworks-Dh2POGVpVbHuSmW8-tjpxsg-t500x500.jpg"></img>
-            </div>
-            <div className="content">
-              <div className="title"><a href="https://soundcloud.com/jlucher/green-hill-zone-chill-electronic-remix" target="_blank">Green Hill Zone - Chill Electronic Mix</a></div>
-              <div className='subtitle'>2022</div>
-              <div className="song-player"><audio controls><source src="music/GHZ-JLucher.mp3" type="audio/mp3"></source></audio></div>
-            </div>
-          </div>
-        </div>
+        <Cards></Cards>
       </header>
     </div>
   );
---
+++ src/components/cards.js
@@ -0,0 +1,43 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+export default class Cards extends React.Component{
+    render(){
+        return(
+            <div className='slider'>
+              <div className="card">
+                <div className="card-img">
+                  <img src="https://i1.sndcdn.com/artworks-Dh2POGVpVbHuSmW8-tjpxsg-t500x500.jpg"></img>
+                </div>
+                <div className="content">
+                  <div className="title"><a href="https://soundcloud.com/jlucher/green-hill-zone-chill-electronic-remix" target="_blank">Green Hill Zone - Chill Electronic Mix</a></div>
+                  <div className='subtitle'>2020</div>
+                  <div className="song-player"><audio controls><source src="music/GHZ-JLucher.mp3" type="audio/mp3"></source></audio></div>
+                </div>
+              </div>
+
+              <div className="card">
+                <div className="card-img">
+                  <img src="https://i1.sndcdn.com/artworks-MPFezXU9ius1hxGF-OEfghg-t240x240.jpg"></img>
+                </div>
+                <div className="content">
+                  <div className="title"><a href="https://m.soundcloud.com/jlucher/order-66-the-finale" target="_blank">Order 66<br/>The Finale Mix</a></div>
+                  <div className='subtitle'>2020</div>
+                  <div className="song-player"><audio controls><source src="music/Order 66 - The Finale.mp3" type="audio/mp3"></source></audio></div>
+                </div>
+              </div>
+
+              <div className="card">
+                <div className="card-img">
+                  <img src="https://i1.sndcdn.com/artworks-Dh2POGVpVbHuSmW8-tjpxsg-t500x500.jpg"></img>
+                </div>
+                <div className="content">
+                  <div className="title"><a href="https://soundcloud.com/jlucher/green-hill-zone-chill-electronic-remix" target="_blank">Green Hill Zone - Chill Electronic Mix</a></div>
+                  <div className='subtitle'>2022</div>
+                  <div className="song-player"><audio controls><source src="music/GHZ-JLucher.mp3" type="audio/mp3"></source></audio></div>
+                </div>
+              </div>
+            </div>      
+        );
+    }
+}
