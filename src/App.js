85a3386b86fe4ee39511a7876f1ecac6811829ba
jll38 <jll38@njit.edu>
2022-11-29 21:15:34 -0500

    Page Elements and Formatting

--- src/App.js
+++ src/App.js
@@ -2,10 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import Cards
  from './components/cards';
+import Nav from './components/nav';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Nav/>
+        <div className='heading'>
+        <h1>Cool Music By Me</h1>
+        <div className='subtext'>The Julian Lechner Experience</div>
+        </div>
         <Cards></Cards>
       </header>
     </div>
---
+++ src/components/nav.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default class Nav extends React.Component{
+    render(){
+        return(
+            <nav>
+                <a href="https://soundcloud.com/jlucher" target="_blank">Soundcloud</a>
+                <a href="https://julianlechner.herokuapp.com/" target="_blank">Portfolio</a>
+            </nav>
+        );
+    }
+}
